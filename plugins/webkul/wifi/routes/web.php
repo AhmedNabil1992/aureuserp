07a6548bc8949d2a407e035f74c43764df792349
AhmedNabil1992 <31648472+AhmedNabil1992@users.noreply.github.com>
2026-04-25 16:13:32 +0300

    feat(wifi): simplify voucher batch download route by removing unnecessary middleware grouping

--- plugins/webkul/wifi/routes/web.php
+++ plugins/webkul/wifi/routes/web.php
@@ -3,11 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Webkul\Wifi\Http\Controllers\VoucherBatchPdfController;
 
-Route::middleware(['web', 'auth'])->group(function (): void {
-    Route::get('wifi/voucher-batches/{batchCode}/download', [VoucherBatchPdfController::class, 'download'])
-        ->where('batchCode', '.*')
-        ->name('wifi.voucher-batches.download');
-});
+Route::get('wifi/voucher-batches/{batchCode}/download', [VoucherBatchPdfController::class, 'download'])
+    ->where('batchCode', '.*')
+    ->middleware(['web'])
+    ->name('wifi.voucher-batches.download');
 
 Route::get('wifi/voucher-batches/{batchCode}/signed-download', [VoucherBatchPdfController::class, 'signedDownload'])
     ->where('batchCode', '.*')
