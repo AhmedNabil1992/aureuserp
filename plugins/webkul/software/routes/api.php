757541741e563b223b580505e531bb85abb2e7f2
AhmedNabil1992 <ahmed.nabil92@outlook.com>
2026-09-13 16:41:28 +0300

    Remove Key

--- plugins/webkul/software/routes/api.php
+++ plugins/webkul/software/routes/api.php
@@ -39,7 +39,8 @@ Route::name('customer.api.v1.software.')
         Route::post('notifications/read-all', [CustomerNotificationController::class, 'markAllAsRead'])->name('notifications.read-all');
     });
 
-Route::prefix('api')->middleware(['App\Http\Middleware\VerifyLegacyApiKey'])->group(function (): void {
+// Route::prefix('api')->middleware(['App\Http\Middleware\VerifyLegacyApiKey'])->group(function (): void {
+Route::prefix('api')->group(function (): void {
     Route::post('/insert-licenses', [LicenseLegacyController::class, 'insertLicenses']);
     Route::post('/insert-keys', [LicenseLegacyController::class, 'insertKeys']);
     Route::post('/license-info', [LicenseLegacyController::class, 'licenseInfo']);
